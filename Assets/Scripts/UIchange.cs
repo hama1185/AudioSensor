36ce1fff15f4cc6231af4b1d3b4e8737f481da06
hama1185 <hamataku7332@softbank.ne.jp>
2020-04-19 17:51:07 +0900

    センサーの値を取得して表示

---
+++ Assets/Scripts/UIchange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIchange : MonoBehaviour
+{
+    public Text accelX;
+    public Text accelY;
+    public Text accelZ;
+    public Text compsX;
+    public Text compsY;
+    public Text compsZ;
+    public Text trueHeading;
+    public Text gyroX;
+    public Text gyroY;
+    public Text gyroZ;
+    public Text gyroW;
+    public GameObject sensorManager;
+    inputcheck inputcheck; 
+    // Start is called before the first frame update
+    void Start(){
+        inputcheck = sensorManager.GetComponent<inputcheck>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        accelX.text = inputcheck.acceleration.x.ToString();
+        accelY.text = inputcheck.acceleration.y.ToString();
+        accelZ.text = inputcheck.acceleration.z.ToString();
+        compsX.text = inputcheck.compass.rawVector.x.ToString();
+        compsY.text = inputcheck.compass.rawVector.y.ToString();
+        compsZ.text = inputcheck.compass.rawVector.z.ToString();
+        trueHeading.text = inputcheck.compass.trueHeading.ToString();
+        gyroX.text = inputcheck.gyro.x.ToString();
+        gyroY.text = inputcheck.gyro.y.ToString();
+        gyroZ.text = inputcheck.gyro.z.ToString();
+        gyroW.text = inputcheck.gyro.w.ToString();
+    }
+}
---
+++ Assets/Scripts/inputcheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputcheck : MonoBehaviour
+{
+    public Vector3 acceleration;//加速度
+    public Compass compass;//磁気
+    public Quaternion gyro;//ジャイロ
+    //なんとなく渡り鳥ARやりたい
+    // Start is called before the first frame update
+    void Start(){
+        Input.compass.enabled = true;
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        acceleration = Input.acceleration;
+        compass = Input.compass;
+        gyro = Input.gyro.attitude;
+    }
+}
